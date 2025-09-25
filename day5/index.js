7064449448761658f18913e00d55a53e6bb36846
Divya21-04 <divyasingh62069@gmail.com>
2025-09-25 22:13:42 +0530

    Add ABES-ELCE-5th folder files

---
+++ day5/index.js
@@ -0,0 +1,13 @@
+//output method in js
+// console.log("Hey! This message comes from html file.");
+// document.writeln("Hey! This message comes from html file");
+// alert("alert message");
+// console.log("error message");
+let userName = prompt("Please enter your name:", "Guest");
+let a = 30;
+a = "abc";
+let b = "name";
+console.log(typeof a, typeof b);
+
+
+    
